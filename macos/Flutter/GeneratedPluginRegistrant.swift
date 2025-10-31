92bd2ff2c4f82f018d78026b2c338da161cdbaf0
Thanh Dat <thanhdatphan725@gmail.com>
2025-10-31 23:35:18 +0700

    Add new Flutter plugins and update dependencies
    Added several new dependencies to pubspec.yaml, including docx_to_text, just_audio, audio_service, shimmer, cached_network_image, syncfusion_flutter_pdf, flutter_animate, animations, audio_waveforms, fluttertoast, lottie, flutter_rating_bar, carousel_slider, dynamic_color, and connectivity_plus. Updated generated plugin registrant files for Linux, macOS, and Windows to register new plugins. Updated pubspec.lock to reflect the new and updated dependencies.

--- macos/Flutter/GeneratedPluginRegistrant.swift
+++ macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,17 +6,29 @@ import FlutterMacOS
 import Foundation
 
 import app_links
+import audio_service
+import audio_session
+import connectivity_plus
+import dynamic_color
 import file_picker
 import google_sign_in_ios
+import just_audio
 import path_provider_foundation
 import shared_preferences_foundation
+import sqflite_darwin
 import url_launcher_macos
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   AppLinksMacosPlugin.register(with: registry.registrar(forPlugin: "AppLinksMacosPlugin"))
+  AudioServicePlugin.register(with: registry.registrar(forPlugin: "AudioServicePlugin"))
+  AudioSessionPlugin.register(with: registry.registrar(forPlugin: "AudioSessionPlugin"))
+  ConnectivityPlusPlugin.register(with: registry.registrar(forPlugin: "ConnectivityPlusPlugin"))
+  DynamicColorPlugin.register(with: registry.registrar(forPlugin: "DynamicColorPlugin"))
   FilePickerPlugin.register(with: registry.registrar(forPlugin: "FilePickerPlugin"))
   FLTGoogleSignInPlugin.register(with: registry.registrar(forPlugin: "FLTGoogleSignInPlugin"))
+  JustAudioPlugin.register(with: registry.registrar(forPlugin: "JustAudioPlugin"))
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
   SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
+  SqflitePlugin.register(with: registry.registrar(forPlugin: "SqflitePlugin"))
   UrlLauncherPlugin.register(with: registry.registrar(forPlugin: "UrlLauncherPlugin"))
 }
